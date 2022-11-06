5ad405098fb397be242d4bea97202530269cc302
Ana Maria Freire <ana.mariap1997@gmail.com>
2022-11-06 18:29:25 -0300

    project finished

---
+++ src/one/digitalinnovation/gof/singleton/SingletonLazy.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation.gof.singleton;
+
+/** 
+ *  Singleton "preguiçoso".
+ * 
+ * 
+ */
+
+public class SingletonLazy {
+	
+	private static SingletonLazy instancia;
+	
+	public SingletonLazy() {
+		super();
+	}
+	
+	public static  SingletonLazy getInstancia () {
+		if(instancia == null) {
+			instancia = new SingletonLazy();
+		}
+		
+		return instancia;
+	}
+}
